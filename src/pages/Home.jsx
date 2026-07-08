7f70be5ee0b81c250a16356874563f7fc331ec2b
PratikAgashe18 <pratikagashe2005@gmail.com>
2026-07-08 09:41:34 +0530

    updated code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import NothHero from "../components/NothHero";
+import NothHero from "../components/Nothhero";
 import BusinessSpectrum from "../components/BusinessSpectrum";
 import Features from "../components/Features";
 import GrowthChart from "../components/GrowthChart";
